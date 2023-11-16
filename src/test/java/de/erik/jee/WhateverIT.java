e231351078b9a95d908556fe2da960613726dfa9
Kai Hennig <khennig@pobox.com>
2023-11-16 21:54:45 +0100

    Add support for Arquillian remote

--- src/test/java/de/erik/jee/WhateverIT.java
+++ src/test/java/de/erik/jee/WhateverIT.java
@@ -1,23 +1,21 @@
 package de.erik.jee;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.inject.Inject;
+
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import javax.ejb.EJB;
-import javax.inject.Inject;
-import java.io.File;
-import java.net.URL;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(Arquillian.class)
 public class WhateverIT {
